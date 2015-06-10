f71ad9749a8fd891c3728b99b910935b56a6b329
jodie <1584834589@qq.com>
2015-06-10 09:29:35 +0800

    文件添加

---
+++ test/test5/index.js
@@ -0,0 +1,45 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: wwz07651
+ * Date: 15-6-9
+ * Time: 下午2:59
+ * To change this template use File | Settings | File Templates.
+ */
+
+//4.1
+//function MyController($scope,$timeout){
+//    var updateClock=function(){
+//        $scope.clock=new Date();
+//        $timeout(function(){
+//            updateClock();
+//        },1000);
+//    };
+//    updateClock();
+//}
+
+//4.2
+function MyController($scope){
+    $scope.clock={
+        now:new Date()
+    }
+    var updateClock=function(){
+        $scope.clock.now=new Date();
+    };
+    setInterval(function(){
+        $scope.$apply(updateClock);
+    },1000);
+    updateClock();
+}
+
+//$rootScope的使用
+angular.module('myApp',[])
+    .run(function($rootScope){
+        $rootScope.name="World";
+    })
+
+//$scope的使用，需创建controller
+angular.module("myApp",[])
+.controller("MyController1",
+function($scope){
+    $scope.name="Ari"
+});
