55990afb5d0805d774f886048acd1a201a1cb530
jodie <1584834589@qq.com>
2015-06-15 09:42:15 +0800

    内容提交

--- test/test6/index.js
+++ test/test6/index.js
@@ -33,25 +33,37 @@
 //    })
 
 //3
-angular.module('emailParse',[])
-    .config(['$interpolateProvider',function($interpolateProvider){
-        $interpolateProvider.startSymbol("__");
-        $interpolateProvider.endSymbol("__");
+angular.module('emailParser', [])
+    .config(['$interpolateProvider',
+    function($interpolateProvider) {
+        $interpolateProvider.startSymbol('__');
+        $interpolateProvider.endSymbol('__');
     }])
-    .factory('EmailParser',['$interpolate',function($interpolate){
-        return{
-            parse:function(text,context){
-                var template=$interpolate(text);
+    .factory('EmailParser', ['$interpolate',
+    function($interpolate) {
+        // a service to handle parsing
+        return {
+            parse: function(text, context) {
+                var template = $interpolate(text);
                 return template(context);
             }
-        }
-    }])
+        };
+    }]);
 
-angular.module('myApp',['emailParser'])
-    .controller('MyController',['$scope','EmailParser',function(body){
-    if(body){
-        $scope.previewText=EmailParser.parse(body,{
-            to:$scope.to
-        });
-    }
-}])
+
+angular.module('myApp', ['emailParser'])
+    .controller('MyController',
+    ['$scope', 'EmailParser',
+        function($scope, EmailParser) {
+//            $scope.to = 'ari@fullstack.io';
+//            $scope.emailBody = 'Hello __to__';
+            // Set up a watch
+            $scope.$watch('emailBody', function(body) {
+                if (body) {
+                    $scope.previewText =
+                        EmailParser.parse(body, {
+                            to: $scope.to
+                        });
+                }
+            });
+        }]);
